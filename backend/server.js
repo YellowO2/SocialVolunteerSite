61fa87f5546239891fbf304a6beeee10ced299bd
Rithwik0I <2200033309@kluniversity.in>
2024-05-24 20:50:58 +0800

    Set up the backend connection to mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,41 @@
 // Import the express module
-const express = require("express");
 // Create an instance of the express application
-const app = express();
+const express = require ('express');
+const cors = require ('cors');
+const mongoose = require('mongoose')
+const app = new express();
 // Specify a port number for the server
 const port = 5000;
-// Start the server and listen to the port
+app.use(express.json());
+app.use(cors());
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://2200033309:Waluigi@cluster0.0fifzmf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+// Start the server and listen to the port
+
